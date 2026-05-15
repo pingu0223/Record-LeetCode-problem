e1ed54d728f191f903a304fe7037a6cfcfa5c1af
pingu0223 <1875894522@qq.com>
2026-05-15 18:27:13 +0800

    Add reverseString function to reverse character vector

--- 26Spring/String/344 reverse String/code.cpp
+++ 26Spring/String/344 reverse String/code.cpp
@@ -8,3 +8,13 @@ void reverseString(vector<char>& s) {
             s[n-i]=temp;
         }
     }
+void reverseString(vector<char>& s) {
+        int n=s.size();
+        int boundary=n/2;
+        int i=0;
+        for(i=0;i<boundary;i++){
+            char temp=s[i];
+            s[i]=s[n-1-i];
+            s[n-i-1]=temp;
+        }
+    }
