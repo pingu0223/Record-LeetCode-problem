fb409a1f374a8825eeccdcec899ac9ae014b45a6
pingu0223 <1875894522@qq.com>
2026-05-17 19:40:54 +0800

    Implement right side view of binary tree

---
+++ 26Spring/Binary Tree/BFS 分层模板/199 二叉树的右视图/code.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>list;
+        if(root==nullptr) return list;
+        queue<TreeNode*>q;
+        q.push(root);
+        int size;
+        while(!q.empty()){
+            size=q.size();
+            list.push_back(q.back()->val);
+            for(int i=0;i<size;i++){
+                TreeNode* current=q.front();
+                if(current->left!=nullptr){
+                    q.push(current->left);
+                }
+                if(current->right!=nullptr){
+                    q.push(current->right);
+                }
+                q.pop();
+            }
+        }
+        return list;
+    }
+};//别忘了右视图是在找q.back();
