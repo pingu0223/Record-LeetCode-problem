5d17c024a5b756f893f679d957353fbacb86ba09
pingu0223 <1875894522@qq.com>
2026-05-17 16:46:45 +0800

    Implement minDepth function for binary tree

---
+++ 26Spring/Binary Tree/Recursive Application/111 二叉树的最小深度/code.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==nullptr) return 0;
+        if(root->left==nullptr&&root->right==nullptr) return 1;
+        if(root->left==nullptr) return minDepth(root->right)+1;
+        if(root->right==nullptr) return minDepth(root->left)+1;
+        int leftMin=minDepth(root->left);
+        int rightMin=minDepth(root->right);
+        return min(leftMin,rightMin)+1;   
+    }
+};
